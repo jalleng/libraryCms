1126b5f8e6985584fd914c238589ee0bdf31cf7f
Jeff Gerber <jallengerber@gmail.com>
2017-03-14 20:54:21 -0700

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 'use strict';
 
 const express = require('express'); 
-const morgan = require('morgan');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const Promise = require('bluebird');
 const debug = require('debug')('library:server.js');
 
+//require routers here
 const errors = require('./lib/error-middleware.js');
 
 dotenv.load();
 
-const PORT = process.env.PORT || 3000;
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/note';
-
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
-
+const PORT = process.env.PORT;
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI);
+
+let morganFormat = process.env.PRODUCTION ? 'common' : 'dev';
+
 app.use(cors);
-app.use(morgan('dev'));
-
-
+app.use(morgan(morganFormat));
 app.use(errors);
+
+
 
 app.listen(PORT, () => {
   debug(`Cthulhu awakened on port: ${PORT}`);
+  console.log(`Cthulhu awakened on port: ${PORT}`);
 });
 
 
