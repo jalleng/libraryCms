3ca53f2f2e1cd8661d76586febe6feb90e7cd20b
Jeff Gerber <jallengerber@gmail.com>
2017-03-16 20:48:08 -0700

    added tests

--- route/book-routes.js
+++ route/book-routes.js
@@ -13,31 +13,34 @@ const bookRouter = module.exports = new Router();
 
 bookRouter.post('/api/book', jsonParser, function(req, res, next) {
   debug('POST: /api/book');
+
+  if (Object.keys(req.body).length === 0) return next(createError(400, 'Expected Body'));
+
   new Book(req.body).save()
   .then( book => res.json(book))
   .catch( err => next(createError(400, err.message)));
 });
 
 
-bookRouter.get('/api/book/:id', function(req, res, next) {
-  debug('GET: /api/book/:id');
-  Book.findById(req.params.id)
+bookRouter.get('/api/book/:bookID', function(req, res, next) {
+  debug('GET: /api/book/:bookID');
+  Book.findById(req.params.bookID)
   .then( book => res.json(book))
   .catch( err => next(createError(404, err.message)));
 });
 
 
-bookRouter.put('/api/book/:id', jsonParser, function(req, res, next) {
-  debug('PUT: /api/book/:id');
-  Book.findByIdAndUpdate(req.params.id, req.body, {new: true})
+bookRouter.put('/api/book/:bookID', jsonParser, function(req, res, next) {
+  debug('PUT: /api/book/:bookID');
+  Book.findByIdAndUpdate(req.params.bookID, req.body, {new: true})
   .then( book => res.json(book))
   .catch( err => next(createError(404, err.message)));
 });
 
 
-bookRouter.delete('/api/book/:id',  jsonParser, function(req, res, next) {
-  debug('DELETE: /api/book/:id');
-  Book.findByIdAndRemove(req.params.Id)
+bookRouter.delete('/api/book/:bookID',  jsonParser, function(req, res, next) {
+  debug('DELETE: /api/book/:bookID');
+  Book.findByIdAndRemove(req.params.bookID)
   .then( () => res.status(204).send())
   .catch ( err => next(createError(404, err.message)));
 });
